bee10513d7930a71e8b83e5ee77309fe0090fee6
npukojiucm <ghbrjkbcn2009>
2022-09-22 21:59:32 +0300

    add hw

---
+++ 3.3-permissions/advertisements/serializers.py
@@ -0,0 +1,50 @@
+from pprint import pprint
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        user = self.context["request"].user
+        count_open_adv = Advertisement.objects.filter(creator=user, status='OPEN').count()
+        method = self.context["request"].method
+
+        if count_open_adv >= 10 and method == 'POST':
+            raise ValidationError('There can be no more than 10 open ads')
+        return data
---
+++ 3.3-permissions/advertisements/views.py
@@ -0,0 +1,29 @@
+from django_filters import DateFromToRangeFilter
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvAdvertisementFilterSet(FilterSet):
+    created_ad = DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['creator', 'created_at']
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvAdvertisementFilterSet
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return [IsOwnerOrReadOnly()]
